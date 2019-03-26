99771c223257f3486305615f24f6be666aecbae3
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-03-25 22:48:05 -0400

    Fix Rol Usuario

---
+++ include/development.php
@@ -0,0 +1,11 @@
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer text-center">
+                Todos los derechos reservados. Dise&ntilde;ado y desarrollado por 
+                <!--<a href="http://cerouno.com.py">CEROUNO Labs</a>.-->
+                TIC'S & GANADERIA S.A.
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
